6e9ad28ed670034d9abd78d077fe15e95e72eaea
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2025-12-05 18:33:31 +0100

    updated Makefile

--- k_cpp/cpp05/ex03/.build/main.d
+++ k_cpp/cpp05/ex03/.build/main.d
@@ -1,4 +1,4 @@
-.build/main.o: main.cpp Classes/Bureaucrat.hpp Classes/Forms/AForm.hpp \
+.build/./main.o: main.cpp Classes/Bureaucrat.hpp Classes/Forms/AForm.hpp \
   Classes/Forms/ShrubberyCreationForm.hpp Classes/Intern.hpp
 Classes/Bureaucrat.hpp:
 Classes/Forms/AForm.hpp:
